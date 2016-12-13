27cd2761fa80ea1d4f20dd0acd5a7759740b2f17
Matthew <thesedatedprince@gmail.com>
2016-12-13 10:48:47 +0000

    Added complete fizzbuzz program

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,11 @@
 def fizzbuzz(number)
-  'fizz'
+  if number % 3 == 0 && number % 5 == 0
+    puts 'Fizzbuzz'
+  elsif number % 3 == 0
+    puts 'Fizz'
+  elsif number % 5 == 0
+    puts 'Buzz'
+  else
+    puts number
+end
 end
